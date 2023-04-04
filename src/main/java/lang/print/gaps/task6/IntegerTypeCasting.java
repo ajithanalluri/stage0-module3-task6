a4f76647f495aeb5556ec5399ae3ef1458b3bf02
EPAM\Ajitha_Nalluri <Ajitha_Nalluri@epam.com>
2023-04-04 14:46:13 +0530

    #1

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.task6;
 
+import java.util.Arrays;
+
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        long first = 1000;
+        int second = (int) first;
+
+        System.out.println(first);
+        System.out.println(second);
     }
 }
